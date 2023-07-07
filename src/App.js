25161543d71115e91654087ce23b5ab742c0b863
Arturo Mendivil <arturo.mendivil@thermofisher.com>
2023-07-07 14:05:12 -0700

    add tailwind and page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ChartJS from 'chart.js/auto';
+import {LinearScale, PointElement, Tooltip, Legend, TimeScale} from "chart.js";
+import { Bar } from 'react-chartjs-2';
+import adapter from 'chartjs-adapter-moment';
+import moment from 'moment';
 
-function App() {
+ChartJS.register(LinearScale, PointElement, Tooltip, Legend, TimeScale);
+
+const baseURL = 'https://www.githubstatus.com/history.atom';
+
+export default function App() {
+  const [incidentData, setIncidentData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(baseURL);
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(response.data, 'application/xml');
+        const entries = xml.getElementsByTagName('entry');
+        const incidents = [];
+
+        // Calculate start date for the last month
+        const startDate = moment().subtract(1, 'month').startOf('month');
+
+
+        for (let i = 0; i < entries.length; i++) {
+          const entry = entries[i];
+
+          // Get title, updated time, and content of incident
+          const title = entry.getElementsByTagName('title')[0].textContent;
+          const updated = entry.getElementsByTagName('updated')[0].textContent;
+          const content = entry.getElementsByTagName('content')[0].textContent;
+
+          // Get date of incident from updated time
+          const date = moment(updated).format('MMM DD, YYYY');
+
+          // Get starting time and ending time of incident from content
+          const timeRangeRegex = /(\d{1,2}:\d{2})(?=.*?var data-var='time'>(\d{1,2}:\d{2}))/g;
+          const timeRangeMatches = Array.from(content.matchAll(timeRangeRegex));
+          const startTime = timeRangeMatches.length > 1 ? timeRangeMatches[1][1] : 'N/A';
+          const endTime = timeRangeMatches.length > 0 ? timeRangeMatches[0][1] : 'N/A';
+          const timeRange = `${startTime} - ${endTime}`;
+
+          // Array of services that failed during incident
+          if (moment(updated).isAfter(startDate)) {
+            incidents.push({
+              title,
+              updated,
+              date,
+              timeRange,
+            });
+          }
+        }
+
+        setIncidentData(incidents);
+      } catch (error) {
+        console.log('Error fetching GitHub status:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+// Count number of incidents per day
+const incidentCountByDate = {};
+incidentData.forEach((incident) => {
+  const { date } = incident;
+  incidentCountByDate[date] = incidentCountByDate[date] ? incidentCountByDate[date] + 1 : 1;
+});
+
+// Graph data using chart.js
+const chartData = {
+  labels: Object.keys(incidentCountByDate),
+  datasets: [
+    {
+      label: 'GitHub Incidents count',
+      data: incidentData.map((incident) => ({
+        x: incident.date,
+        y: incidentCountByDate[incident.date],
+      })),
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+    },
+  ],
+};
+
+const chartOptions = {
+  scales: {
+    x: {
+      type: 'time',
+      adapters: {
+        date: {
+          locale: 'en',
+        },
+      },
+      time: {
+        unit: 'day',
+        displayFormats: {
+          day: 'MMM D',
+        },
+      },
+      title: {
+        display: true,
+        text: 'Date',
+      },
+    },
+  },
+  plugins: {
+    tooltip: {
+      callbacks: {
+        label: (context) =>
+          `${incidentData[context.dataIndex].timeRange} \n, ${incidentData[context.dataIndex].title}`,
+      },
+    },
+  },
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex md:h-screen flex-col m-0 p-0 items-center transform rotate-90 md:transform-none md:rotate-0'>
+      <div className='flex flex-col h-full md:w-full md:h-fit'>
+        <h1 className='h-full md:w-full md:h-fit text-3xl text-center px-4 py-3 border-b border-slate-300'>GitHub Status</h1>
+        <p className='text-center m-2'>Incidents in the last month</p>
+      </div>
+      <div className='h-full md:w-full flex flex-1 md:h-[calc(100vh-80px)] justify-center p-6'>
+        <Bar data={chartData} options={chartOptions}/>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
